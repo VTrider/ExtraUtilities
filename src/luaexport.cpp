37abfa3fa09b1a49c260aefe2e069c254c0ffaa3
VTrider <157431961+VTrider@users.noreply.github.com>
2024-07-23 16:40:27 -0400

    Updated licensing and copyright information

--- src/luaexport.cpp
+++ src/luaexport.cpp
@@ -12,6 +12,24 @@
 #include "lua.hpp"
 #include <Windows.h>
 
+/* Copyright (C) 2023-2024 VTrider
+ *
+ * This file is part of Extra Utilities.
+ *
+ * Extra Utilities is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <chrono>
 #include <thread>
 #include "Log.h"
